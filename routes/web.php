31bb77f2bbc7462a8ab214ea9ac71f0f6e16c5c0
syukriothman10 <syukriothman10@gmail.com>
2025-05-29 00:06:51 +0800

    Add show route for showing company details

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+    
     //companies routes
     Route::get('/companies', [CompaniesController::class, 'index'])->name('companies.index');
     Route::get('/companies/create', [CompaniesController::class, 'create'])->name('companies.create');
     Route::post('/companies/store', [CompaniesController::class, 'store'])->name('companies.store');
+    Route::get('/companies/show{id}', [CompaniesController::class, 'show'])->name('companies.show');
     Route::get('/companies/edit{id}', [CompaniesController::class, 'edit'])->name('companies.edit');
     Route::put('/companies/edit{id}', [CompaniesController::class, 'update'])->name('companies.update');
     Route::delete('/companies/destroy{id}', [CompaniesController::class, 'destroy'])->name('companies.destroy');

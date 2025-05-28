42cecf16fe83899b5e7aee0c6a6d6889a91fa576
syukriothman10 <syukriothman10@gmail.com>
2025-05-28 23:23:55 +0800

    Update companies table migration: remove unique from name and add unique to website

--- database/migrations/2025_05_28_132650_create_companies_table.php
+++ database/migrations/2025_05_28_132650_create_companies_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('logo')->nullable();
-            $table->string('website')->nullable();
+            $table->string('website')->nullable()->unique();
             $table->timestamps();
         });
     }
